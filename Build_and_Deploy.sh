bf3c19c76a1268802f0cab461776840897d52df0
Paul Boxwell <pmboxwell@gmail.com>
2022-01-05 22:32:54 +0000

    Updated port

--- Build_and_Deploy.sh
+++ Build_and_Deploy.sh
@@ -4,4 +4,4 @@
 #   git clone https://github.com/paulboxwell/simpleHTMLdocker.git
 
 docker build -t my-apache2 .
-docker run -dit --name my-running-app -p 8080:80 my-apache2
+docker run -dit --name my-running-app -p 80:80 my-apache2
